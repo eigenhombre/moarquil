167ce084d10fbc2084977398a513d94e83cd3df4
John Jacobsen <john@mail.npxdesigns.com>
2015-10-26 19:04:40 -0500

    Remove currently unneeded Component stuff;
    implement better Quil app lifecycle at REPL.

--- project.clj
+++ project.clj
@@ -2,12 +2,5 @@
   :description ""
   :url "https://github.com/eigenhombre/moarquil"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [quil "2.2.6"]
-                 [com.stuartsierra/component "0.3.0"]
-                 [org.clojure/tools.namespace "0.2.11"]]
-  :aliases {"autotest" ["spec" "-a"]}
-  :plugins [[speclj "3.3.0"]]
-  :test-paths ["spec"]
-  :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[speclj "3.3.0"]]}
-             :uberjar {:aot :all}})
+                 [quil "2.2.6"]]
+  :profiles {:uberjar {:aot :all}})
--- src/moarquil/core.clj
+++ src/moarquil/core.clj
@@ -1,49 +1,58 @@
 (ns moarquil.core
-  (:require [quil.core :as q]
-            [quil.middleware :as m]))
+  (:require [quil.core :refer :all]
+            [quil.helpers.drawing :refer [line-join-points]]
+            [quil.helpers.calc :refer [mul-add]]))
 
 
-(comment
-  (-> '(defsketch foo)
-      macroexpand-1
-      macroexpand-1)
-  ;;=>
 
-  
+(defonce app (atom nil))
+
+
+;; http://stackoverflow.com/questions/12545570/\
+;; how-to-destroy-processing-papplet-without-calling-exit
+;;(.destroy foo) ;; Sometimes kills REPL process
+(when @app
+  (.setVisible (.frame @app) false)
+  (reset! app nil))
+
+
+(def radius 400)
+
+
 (defn setup []
-  (q/smooth)                          ;; Turn on anti-aliasing
-  (q/frame-rate 100)                    ;; Set framerate to 1 FPS
-  (q/background 200))                 ;; Set the background colour to
-;; a nice shade of grey.
-
-  (defn draw []
- (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
-
-  (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-    (q/ellipse x y diam diam)))
-  (def foo (quil.applet/applet
-            :size [400 400]
-            :setup setup
-            :draw draw))
-  
-  (-> foo .exit)
+  (background 255)
+  (stroke 00))
 
 
+(defn ^:private lissajeux-line []
+  (for [t (map (partial * 0.05) (range 0 3610))]
+    (let [s (* t 100)
+          radian-s (radians s)
+          radian-t (radians t)
+          x (* radius  (cos radian-s) (sin radian-t))
+          y (* radius  (sin radian-s) (sin radian-t))
+          z (* radius (cos radian-t))]
+      [x y z])))
 
 
+(defn- render [lines]
+  (doseq [l lines]
+    (dorun
+     (map (partial apply line) (line-join-points l)))))
 
 
+(defn draw []
+  (background 250)
+  (translate (/ (width) 2) (/ (height) 2) 0)
+  (rotate-y (* (frame-count) 0.0005))
+  (rotate-x (* (frame-count) 0.0025))
+  (render [(lissajeux-line)]))
 
 
-
-
-
-
-
-  )
-
-
+(let [thisapp (quil.applet/applet
+               :size [840 1200]
+               :setup setup
+               :draw draw
+               :renderer :opengl)]
+  (.setLocation (.frame thisapp) 0 0)
+  (reset! app thisapp))
